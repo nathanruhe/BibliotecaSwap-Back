d42e828e6b3c5513886fb065cf10858a53cc3d85
nathanruhe <nathan.ruhe@hotmail.com>
2024-08-07 18:13:41 +0200

    correcion

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 const app = require("./src/app")
 
-app.listen(app.get("port"), function () {
-    console.log("Server listen on port " + app.get("port"));
-});
+app.listen(process.env.PORT || 3000);
