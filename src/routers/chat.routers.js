627aef3bffc870091ddac2325c8ef267df6013aa
OddQueen <queensleymei@gmail.com>
2024-09-13 08:17:00 +0100

    reordeno los endpoints

--- src/routers/chat.routers.js
+++ src/routers/chat.routers.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { enviarMensaje, obtenerMensajes, obtenerChatsUsuario, getChatUser } = require("../controller/chat.controller");
 
+router.get('/obtenerChatsUsuario/:id_user', obtenerChatsUsuario);
 router.get('/obtenerMensajes/:id_chat', obtenerMensajes);
 router.post('/enviarMensaje', enviarMensaje);
-router.get('/obtenerChatsUsuario/:id_user', obtenerChatsUsuario);
 
 
 module.exports = router;
